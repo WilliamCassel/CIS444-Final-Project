8286cfc179c5016bbdfad99fccee2ad13fc6b9dd
WilliamCassel <casse017@csusm.edu>
2023-11-27 17:50:40 -0800

    added submitFile.php which currently only works for mirage
    This now connects to the database and adds the users explanation and video to the database, and then reloads the webpage to get rid of the form which was appended. Currently it only reroutes to mirage.html, need to fix that using session cookies or create 7 other submitFile.php, one for each map

---
+++ CIS444Project/submitFile.php
@@ -0,0 +1,39 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $explanation = $_POST['explanation'];
+
+    $targetDirectory = "uploads/";
+    $targetFile = $targetDirectory . basename($_FILES["videoFile"]["name"]);
+    $uploadOk = 1;
+    
+ 
+    
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    } else {
+        if (move_uploaded_file($_FILES["videoFile"]["tmp_name"], $targetFile)) {
+            $connString = "mysql:host=localhost;dbname=smoke_submissions";
+            $user = "will";
+            $pass = "will";
+
+            $pdo = new PDO($connString, $user, $pass);
+
+            $sql = "INSERT INTO submissions (explanation, video_file) VALUES (?, ?)";
+            $stmt = $pdo->prepare($sql);
+            
+            // Bind parameters by reference
+            $stmt->bindParam(1, $explanation);
+            $stmt->bindParam(2, $targetFile);
+            
+            if ($stmt->execute()) {
+                echo "Data inserted successfully.";
+		header("Location: mirage.html"); //need to make individual submitFile.php's for each map, or maybe use session cookies for the map name and then append .html to the end and we can just do it from one submitFile.php
+		exit();
+	
+            } else {
+                echo "Error inserting data.";
+            }
+        }
+    }
+}
+?>
